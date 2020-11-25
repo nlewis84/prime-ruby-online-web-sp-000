0447477be5364f1a84814d83cb3173c648be2d6f
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 05:18:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+require 'pry'
+
+def prime?(n)
+  return false if (n <= 1)
+  return true if (n <= 3)
+  return false if (n % 2 == 0 || n % 3 == 0)
+  i = 5
+  while i*i <= n do
+    return false if n % i == 0 || n % (i + 2) == 0
+    i += 6
+  end
+  return true
+end
