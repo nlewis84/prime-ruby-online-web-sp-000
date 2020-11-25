caae25c55eb88aa3d1b1635e3699d4bd803d0ce3
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 05:17:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,7 @@ def prime?(n)
   return true if (n <= 3)
   return false if (n % 2 == 0 || n % 3 == 0)
   i = 5
-  while i*i <= n do |index|
+  while i*i <= n do
     return false if n % i == 0 || n % (i + 2) == 0
     i += 6
   end
