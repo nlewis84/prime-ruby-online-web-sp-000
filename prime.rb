c5c39c32a392934085f9e44a9562238b77673831
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 04:59:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,3 +4,13 @@ require 'pry'
 def prime?(int)
   
 end
+
+let A be an array of Boolean values, indexed by integers 2 to n,
+    initially all set to true.
+    
+    for i = 2, 3, 4, ..., not exceeding √n do
+        if A[i] is true
+            for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n do
+                A[j] := false
+
+    return all i such that A[i] is true.
